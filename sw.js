31b76f63b52994432b981821cd90890431ec033a
gkkbtrf4rv-cloud <gkkbtrf4rv@privaterelay.appleid.com>
2026-09-11 09:33:27 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cancionero-tuna-derecho-v49";
+const CACHE_NAME = "cancionero-tuna-derecho-v50";
 const UPDATE_MARKER_URL = "/__cancionero_update_marker__";
 const ASSETS_TO_CACHE = ["/", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
